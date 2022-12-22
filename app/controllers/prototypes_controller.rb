a23fd01e66b10606dece9bc116b4415ba6873c11
shindo244244 <t.shindo244@gmail.com>
2022-12-22 11:26:32 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -52,8 +52,6 @@ class PrototypesController < ApplicationController
   end
   
   def move_to_index
-    unless current_user == @prototype_user
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
